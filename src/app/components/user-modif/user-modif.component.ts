c32baf482c02390ea3f069c51c46986f33774f88
JACaminero <juanc-r1@hotmail.com>
2021-10-31 11:44:30 -0400

    NOOOOOOOOOOOOO

--- src/app/components/user-modif/user-modif.component.ts
+++ src/app/components/user-modif/user-modif.component.ts
@@ -10,33 +10,37 @@ import { User } from 'src/app/models/models';
   styleUrls: ['./user-modif.component.scss']
 })
 export class UserModifComponent implements OnInit {
-  
+
   userForm = new FormGroup({
     firstName: new FormControl('', [Validators.maxLength(30), Validators.required]),
     lastName1: new FormControl('', [Validators.maxLength(30), Validators.required]),
     lastName2: new FormControl('', [Validators.maxLength(30), Validators.required]),
     birth: new FormControl({}, [Validators.required]),
     cedula: new FormControl('', [
-      Validators.required, Validators.pattern(/^[0-9]{3}-?[0-9]{7}-?[0-9]{1}$/),Validators.maxLength(11), Validators.minLength(11)
+      Validators.required, Validators.pattern(/^[0-9]{3}-?[0-9]{7}-?[0-9]{1}$/),
+      Validators.maxLength(11), Validators.minLength(11)
     ]),
     phone: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
-    grado: new FormControl('', [Validators.required]),
-    seccion: new FormControl('A', [Validators.required])
+    grado: new FormControl(''),
+    seccion: new FormControl('A')
   });
 
   uss: User[] = []
   id: unknown
+  emailOriginal = ''
   constructor(private uServ: UserService, private route: ActivatedRoute) {
     uServ.getAll().subscribe(us => this.uss = us)
-   }
+  }
 
   current: User = new User()
   dat?: string
 
   ngOnInit(): void {
-    this.id = this.route.snapshot.paramMap.get('id');
+    this.id = this.route.snapshot.paramMap.get('id')
+
     this.uServ.getById(<number>this.id).subscribe(u => {
+      this.emailOriginal = u.username
       this.current = u;
       this.userForm.patchValue({
         firstName: this.current.name,
@@ -48,7 +52,7 @@ export class UserModifComponent implements OnInit {
         grado: this.current.grado,
         seccion: this.current.seccion
       })
-    });
+    })
   }
 
 
@@ -58,18 +62,24 @@ export class UserModifComponent implements OnInit {
       alert('Han ocurrido problemas con la informacion ingresa')
       return;
     }
+
+    if (this.current.role == 'Estudiante') {
+      this.userForm.controls.cedula.clearValidators();
+      this.userForm.controls.cedula.updateValueAndValidity();
+    }
+
     this.current.name = this.userForm.controls.firstName.value;
     this.current.lastName1 = this.userForm.controls.lastName1.value;
     this.current.lastName2 = this.userForm.controls.lastName2.value;
     this.current.username = this.userForm.controls.email.value;
     this.current.cedula = this.userForm.controls.cedula.value;
     this.current.phone = this.userForm.controls.phone.value;
-    this.current.birth = this.userForm.controls.birth.value  == {} ? this.current.birth : this.userForm.controls.birth.value;
+    this.current.birth = this.userForm.controls.birth.value == {} ? this.current.birth : this.userForm.controls.birth.value;
     this.current.grado = this.userForm.controls.grado.value;
     this.current.seccion = this.userForm.controls.seccion.value;
 
     this.uss.forEach(us => {
-      if (us.username == this.current.username) {
+      if (this.emailOriginal != this.current.username && us.username == this.current.username) {
         alert('Este E-mail ya esta registrado en el sistema.')
         return;
       }
@@ -77,25 +87,6 @@ export class UserModifComponent implements OnInit {
 
     this.uServ.modify(this.current).subscribe(resp => {
       alert(resp.message)
-    });
-  }
-
-  formatDate(originalDate: Date) {
-    let date = new Date(originalDate);
-    let year = date.getFullYear();
-    let month = date.getMonth() + 1;
-    let day = date.getDate();
-
-    let strDay = date.getDay().toString();
-    let strMonth = date.getDay().toString();
-
-    if (day < 10) {
-      strDay = '0' + day;
-    }
-    if (month < 10) {
-      strMonth = '0' + month;
-    }
-
-    return year + '-' + strMonth + '-' + strDay
+    })
   }
 }
